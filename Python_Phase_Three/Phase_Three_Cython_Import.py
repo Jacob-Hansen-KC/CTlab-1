c2fa188c634d5a5a4ebb85655cfdb98c7310dc2e
Jacob Hansen <jacobhansen.kc@gmail.com>
2025-06-16 07:24:06 -0500

    Add files via upload

---
+++ Python_Phase_Three/Phase_Three_Cython_Import.py
@@ -0,0 +1,5 @@
+import Phase_Three_Cython_2
+import numpy as np
+bound=np.array([277,283,280,280],dtype=np.float64)
+#bound=np.array([150,300,225,225])
+Phase_Three_Cython_2.Phase_Three_Cython(bound)
