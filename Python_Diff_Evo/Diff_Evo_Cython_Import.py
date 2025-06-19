f298163f1a53fe3eaa5143355610386f54c95a45
Jacob Hansen <jacobhansen.kc@gmail.com>
2025-06-19 08:38:05 -0500

    Add files via upload

--- Python_Diff_Evo/Diff_Evo_Cython_Import.py
+++ Python_Diff_Evo/Diff_Evo_Cython_Import.py
@@ -1,12 +1,15 @@
 
 import time
-import Diff_Evo_Cython_2
+import Diff_Evo_Cython_3
 import numpy as np
 total=0
 start = time.time()
 for i in range(100):
-    count=Diff_Evo_Cython_2.Diff_Evo_Cython()
-    total=count+total
+    count=Diff_Evo_Cython_3.Diff_Evo_Cython()
+    #total=count+total
 end = time.time()
-print(end - start, "seconds")
-print(total)
+elapsed = end - start
+print(elapsed, "seconds")
+print(count)
+ips= elapsed /100/count
+print(ips, "seconds per iteration")
