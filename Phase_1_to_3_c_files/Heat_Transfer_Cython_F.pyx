f7dc9514c1b2d47cd4103c93c0969763b672893a
Jacob Hansen <jacobhansen.kc@gmail.com>
2025-06-19 09:21:43 -0500

    Add files via upload

--- Phase_1_to_3_c_files/Heat_Transfer_Cython_F.pyx
+++ Phase_1_to_3_c_files/Heat_Transfer_Cython_F.pyx
@@ -1,27 +1,15 @@
 # cython: boundscheck=False, wraparound=False, cdivision=True
 import numpy as np
 cimport numpy as np
-def Heat_Transfer_Cython(bdry):
 
-    cdef double t, alpha, dx, dy, dt, value
-    cdef int s, k
-    cdef np.ndarray[np.float64_t, ndim=2] T2, T1, S
-    # define simple constants
-    a = 97  # mm^2/s
-    alpha = a / 1000**2  # m^2/s
-
-    # area matrix, length, time parameters
-    s = 20
-    dx = 0.01
-    dy = 0.01
-    dt = 0.1
-
-    # initial temperature and heat source
-    T2 = np.ones((s+2, s+2)) * 273.15  # K
-    T1 = np.zeros((s+2, s+2))
-    S = np.zeros((s+2, s+2))
-    S[11, 2] = 10
-    k = 0
+def Heat_Transfer_Cython(np.ndarray[np.float64_t, ndim=1] bdry):
+    cdef double alpha = 97.0 / 1_000_000.0  # m^2/s
+    cdef int s = 20, k
+    cdef double dx = 0.01, dy = 0.01, dt = 0.1
+    cdef np.ndarray[np.float64_t, ndim=2] T2 = np.ones((s+2, s+2), dtype=np.float64) * 273.15
+    cdef np.ndarray[np.float64_t, ndim=2] T1 = np.zeros((s+2, s+2), dtype=np.float64)
+    cdef np.ndarray[np.float64_t, ndim=2] S = np.zeros((s+2, s+2), dtype=np.float64)
+    S[11, 2] = 10.0
 
     # boundary conditions: left, bottom, right, top
     T2[:, 0] = bdry[0]
@@ -29,19 +17,17 @@ def Heat_Transfer_Cython(bdry):
     T2[:, -1] = bdry[2]
     T2[-1, :] = bdry[3]
 
-    # main loop
-    while k < 1200:
-        T1 = T2.copy()
-        # Explicit Euler scheme
+    for k in range(1200):
+        T1[:, :] = T2
+        # Explicit Euler scheme (vectorized)
         T2[1:-1, 1:-1] = (
             T1[1:-1, 1:-1]
             + alpha * dt * (
-                (T1[2:, 1:-1] - 2*T1[1:-1, 1:-1] + T1[:-2, 1:-1]) / dx**2
-                + (T1[1:-1, 2:] - 2*T1[1:-1, 1:-1] + T1[1:-1, :-2]) / dy**2
+                (T1[2:, 1:-1] - 2.0 * T1[1:-1, 1:-1] + T1[:-2, 1:-1]) / dx**2
+                + (T1[1:-1, 2:] - 2.0 * T1[1:-1, 1:-1] + T1[1:-1, :-2]) / dy**2
             )
             + S[1:-1, 1:-1] * dt
         )
-        k += 1
         # reapply boundary conditions
         T2[:, 0] = bdry[0]
         T2[0, :] = bdry[1]
