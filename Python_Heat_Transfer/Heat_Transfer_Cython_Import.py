eaf27a861706ad50e948a41cf7c9117ff892d8d6
Jacob Hansen <jacobhansen.kc@gmail.com>
2025-06-20 11:18:50 -0500

    Add files via upload

--- Python_Heat_Transfer/Heat_Transfer_Cython_Import.py
+++ Python_Heat_Transfer/Heat_Transfer_Cython_Import.py
@@ -1,10 +1,10 @@
 import time
-import Heat_Transfer_Cython_2
+import Heat_Transfer_Cython_3
 import numpy as np
 bound=np.array([500,300,200,700], dtype=np.float64)
 start = time.time()
 #for i in range(1):
-T2 = Heat_Transfer_Cython_2.Heat_Transfer_Cython(bound)
+T2 = Heat_Transfer_Cython_3.Heat_Transfer_Cython(bound)
 end = time.time()
 print(end - start, "seconds")
 print(T2)
