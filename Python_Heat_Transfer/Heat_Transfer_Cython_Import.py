737c5e4adbc99a724bfb3056873a05251c5f0a08
Jacob Hansen <jacobhansen.kc@gmail.com>
2025-07-09 07:43:38 -0500

    Add files via upload

--- Python_Heat_Transfer/Heat_Transfer_Cython_Import.py
+++ Python_Heat_Transfer/Heat_Transfer_Cython_Import.py
@@ -1,12 +1,14 @@
 import time
-import Heat_Transfer_Cython_4 as Heat_Transfer_Cython
+import Heat_Transfer_Cython_5 as Heat_Transfer_Cython
 import numpy as np
 bound=np.array([500,200,300,700], dtype=np.float64)
 start = time.time()
 #for i in range(1):
-T2,k= Heat_Transfer_Cython.Heat_Transfer_Cython(bound)
+T2,k,timet= Heat_Transfer_Cython.Heat_Transfer_Cython()
 end = time.time()
-print(end - start, "seconds")
+print(timet, "seconds")
+#np.savetxt("T2_output.csv", T2, delimiter=",", fmt="%.2f")
+#print("Saved to T2_output.csv")
 #for row in T2:
     #print(row)
 print(k)
